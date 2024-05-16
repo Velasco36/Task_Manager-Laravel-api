7af0e4c4985e1de4b787970ae9d32168326b997c
Miguel Velasco <migue16velasco@gmail.com>
2024-05-16 16:25:05 -0400

    feat: add userProfile router

--- api-project/routes/api.php
+++ api-project/routes/api.php
@@ -15,10 +15,11 @@ use App\Http\Controllers\API\UserController;
 |
 */
 
-Route::group(['middleware'=> 'api'], function($routes){
+Route::group(['middleware' => 'api'], function ($routes) {
     Route::post('/register', [UserController::class, 'register']);
     Route::post('/login', [UserController::class, 'login']);
     Route::get('/logout', [UserController::class, 'logout']);
+    Route::get('/userProfile', [UserController::class, 'userProfile']);
 });
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
