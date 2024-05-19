c4021a8603cade0b7f7d800b816c32ddf67c6e40
Miguel Velasco <migue16velasco@gmail.com>
2024-05-19 06:41:32 -0400

    feat: add reset password and update profile

--- Notas/src/redux/actions/actionsTypes.js
+++ Notas/src/redux/actions/actionsTypes.js
@@ -1,3 +1,7 @@
 export const GET_USER = 'GET_USER'
 export const REGISTER = 'REGISTER'
 export const SIGNIN = 'SIGNIN'
+export const RESET_PASSWORD = 'RESET_PASSWORD'
+export const UPDATE_PROFILE = 'UPDATE_PROFILE'
+
+
