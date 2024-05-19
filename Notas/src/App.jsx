ab52182193b01cde002fcb66db1f7a124a9e7da5
Miguel Velasco <migue16velasco@gmail.com>
2024-05-19 06:29:21 -0400

    feat: add router profile forgorpassword and home

--- Notas/src/App.jsx
+++ Notas/src/App.jsx
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/layout";
 import Register from "./views/Register/Register";
 import Login from "./views/Login/Login";
+import Index from "./views/Home/Index";
+import ForgotPassword from "./views/ForgotPassword/ForgotPassword";
+import Profile from "./views/User/Profile";
 
 function App() {
   return (
-    // Asegúrate de retornar JSX aquí
     <Routes>
+      <Route element={<Layout />}>
+
+        <Route path="/" element={<Index />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
     </Routes>
   );
 }
