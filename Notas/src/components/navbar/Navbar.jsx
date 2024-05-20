e413b4286f9ad83ec60d2cbbd5e80af800a945b4
Miguel Velasco <migue16velasco@gmail.com>
2024-05-20 16:31:04 -0400

    feat: delete ico menu

--- Notas/src/components/navbar/Navbar.jsx
+++ Notas/src/components/navbar/Navbar.jsx
@@ -16,7 +16,6 @@ export default function Navbar() {
   const { message } = user;
 
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     if (formattedToken) {
@@ -29,9 +28,6 @@ export default function Navbar() {
     setIsUserMenuOpen(!isUserMenuOpen);
   };
 
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
 
   const handleLogout = async(e) => {
      e.preventDefault();
@@ -64,7 +60,7 @@ export default function Navbar() {
 
   return (
     <div>
-      <nav className="bg-white border-gray-100 dark:bg-gray-300 h-18 shadow-2xl">
+      <nav className="bg-white dark:bg-white shadow-2xl h-18 ">
         <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
           <p className="text-1xl font-weight-lighter text-center">
             Administrador de Tareas
@@ -77,7 +73,7 @@ export default function Navbar() {
               aria-expanded={isUserMenuOpen}
               onClick={toggleUserMenu}
             >
-              <AccountCircleIcon className="text-white " />
+              <AccountCircleIcon className="text-black " />
             </button>
 
             <Transition
@@ -118,7 +114,7 @@ export default function Navbar() {
                       Settings
                     </Link>
                   </li>
-                 
+
                   <li>
                     <a
                       onClick={handleLogout}
@@ -131,84 +127,9 @@ export default function Navbar() {
                 </ul>
               </div>
             </Transition>
-            <button
-              data-collapse-toggle="navbar-user"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-user"
-              aria-expanded={isMobileMenuOpen}
-              onClick={toggleMobileMenu}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15" />
-              </svg>
-            </button>
+
           </div>
-          <Transition
-            show={isMobileMenuOpen}
-            enter="transition ease-out duration-100 transform"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="transition ease-in duration-75 transform"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <div
-              className="items-center justify-between w-full md:flex md:w-auto md:order-1"
-              id="navbar-user"
-            >
-              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Services
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Pricing
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </Transition>
+
         </div>
       </nav>
     </div>
