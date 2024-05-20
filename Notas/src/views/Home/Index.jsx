63f10ef2502f93a9684d0978d2695b2bbcdba32a
Miguel Velasco <migue16velasco@gmail.com>
2024-05-20 16:33:54 -0400

    feat: desing templete index

--- Notas/src/views/Home/Index.jsx
+++ Notas/src/views/Home/Index.jsx
@@ -1,10 +1,31 @@
-
-
-const Index =() => {
+import Button from "@mui/material/Button";
+import NoteList from "../Note/List/NoteList";
+const Index = () => {
   return (
-    <div className="bg-blue-50 h-screen"><h1>hello worldd</h1></div>
-  )
-}
+    <div className="bg-blue-50 flex flex-col  items-center justify-between px-4 py-6">
+      <div>
+        <p className="text-3xl text-start">Tablero de tareas ordenar por:</p>
+        <div className="flex space-x-2 justify-center mt-5">
+          <Button
+            variant="outlined"
+            className="rounded-full px-4 py-2 hover:bg-gray-200"
+          >
+            Título
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className="rounded-full px-4 py-2 hover:bg-gray-200"
+          >
+            Fecha
+          </Button>
+        </div>
+      </div>
 
+      {/* Note List */}
+      <NoteList />
+    </div>
+  );
+};
 
 export default Index;
