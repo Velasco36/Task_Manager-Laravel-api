58f62100325e587a6a5e457a584bfa6248d92714
Miguel Velasco <migue16velasco@gmail.com>
2024-05-21 07:50:28 -0400

    feat: add last_name and user

--- Notas/src/views/Register/Register.jsx
+++ Notas/src/views/Register/Register.jsx
@@ -4,20 +4,25 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { register } from "../../redux/actions/actions";
 import Swal from "sweetalert2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const [input, setInput] = useState({
-    nombre: "",
+    name: "",
+    last_name: "",
+    username: "",
     email: "",
     password: "",
-    passwordConfimm: "",
+    password_confirmation: "",
   });
 
-  const [showConfirmPassword, setShowConfimPassword] = useState(false);
- console.log(setShowConfimPassword)
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   const handleChange = (event) => {
     setInput({
       ...input,
@@ -27,16 +32,16 @@ const Register = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const newUser = { ...input };
-    if (input.passwordConfimm !== input.password) {
+
+    if (input.password_confirmation !== input.password) {
       Swal.fire({
-        title: "Verifica la informacion.",
+        title: "Verifica la información.",
         text: "Por favor, verifique que las contraseñas sean iguales",
         icon: "warning",
       });
     } else {
       try {
-        await dispatch(register(newUser));
+        await dispatch(register(input));
         Swal.fire({
           position: "center",
           icon: "success",
@@ -44,17 +49,23 @@ const Register = () => {
           showConfirmButton: false,
           timer: 2000,
         });
-        setInput({
-          nombre: "",
-          email: "",
-          password: "",
-          passwordConfimm: "",
-        });
+
         navigate("/login");
       } catch (error) {
+        const errorData = error.response.data;
+        let errorMessage = "";
+
+        if (errorData.username) {
+          errorMessage += `Username: ${errorData.username.join(", ")}\n`;
+        }
+
+        if (errorData.email) {
+          errorMessage += `Email: ${errorData.email.join(", ")}`;
+        }
         Swal.fire({
           icon: "error",
-          title: "Este usuario ya esta registrado.",
+          title: "Error al registrar usuario",
+          text: errorMessage || "Ocurrió un error inesperado",
         });
       }
     }
@@ -70,17 +81,18 @@ const Register = () => {
         />
       </div>
       <div className="flex w-full flex-col md:w-1/2">
-        <div className="my-auto rounded-lg h-3/5 shadow-2xl mx-auto  flex flex-col justify-center px-6 pt-8 md:justify-start lg:w-[28rem]">
+        <div className="my-auto rounded-lg h-4/5 shadow-2xl mx-auto flex flex-col justify-center px-6 pt-8 md:justify-start lg:w-[32rem]">
           <p className="text-center text-3xl font-bold md:text-left md:leading-tight">
-            Create your free account
+            Crea tu cuenta
           </p>
           <p className="mt-6 text-center font-medium md:text-left">
-            Already using wobble?
+            Ya tienes cuenta
             <Link
               to="/login"
               className="whitespace-nowrap font-semibold text-blue-700"
             >
-              Login here
+              {" "}
+              Inicia Sesión aquí
             </Link>
           </p>
 
@@ -93,10 +105,36 @@ const Register = () => {
                 <input
                   type="text"
                   id="login-name"
-                  name="nombre"
+                  name="name"
                   className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                  placeholder="Name"
-                  value={input.nombre}
+                  placeholder="Nombre"
+                  value={input.name}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col pt-4">
+              <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                <input
+                  type="text"
+                  id="login-apellido"
+                  name="last_name"
+                  className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                  placeholder="Apellido"
+                  value={input.last_name}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col pt-4">
+              <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                <input
+                  type="text"
+                  id="login-username"
+                  name="username"
+                  className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                  placeholder="Usuario"
+                  value={input.username}
                   onChange={handleChange}
                 />
               </div>
@@ -117,7 +155,7 @@ const Register = () => {
             <div className="mb-4 flex flex-col pt-4">
               <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   id="login-password"
                   name="password"
                   className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
@@ -125,6 +163,12 @@ const Register = () => {
                   value={input.password}
                   onChange={handleChange}
                 />
+                <span
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                </span>
               </div>
             </div>
 
@@ -133,12 +177,20 @@ const Register = () => {
                 <input
                   type={showConfirmPassword ? "text" : "password"}
                   id="login-password-confirm"
-                  name="passwordConfimm"
+                  name="password_confirmation"
                   className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
                   placeholder="Confirm Password"
-                  value={input.passwordConfimm}
+                  value={input.password_confirmation}
                   onChange={handleChange}
                 />
+                <span
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  <FontAwesomeIcon
+                    icon={showConfirmPassword ? faEyeSlash : faEye}
+                  />
+                </span>
               </div>
             </div>
 
