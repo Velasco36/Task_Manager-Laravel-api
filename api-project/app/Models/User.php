75014d7e8f203f224e3f8968bda6423bc7f00465
Miguel Velasco <migue16velasco@gmail.com>
2024-05-21 17:54:24 -0400

    feat: move username

--- api-project/app/Models/User.php
+++ api-project/app/Models/User.php
@@ -31,8 +31,8 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $fillable = [
         'name',
-        'username',
         'last_name',
+        'username',
         'email',
         'password',
     ];
